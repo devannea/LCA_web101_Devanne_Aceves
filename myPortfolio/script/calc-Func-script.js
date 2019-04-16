59e193bff11f29552b0915fcc1c8c77bcff1b2ac
devannea <devannenotes@gmail.com>
2019-04-15 20:21:19 -0500

    Made calculator functional

---
+++ myPortfolio/script/calc-Func-script.js
@@ -0,0 +1,42 @@
+function addNumber(num) {
+    document.querySelector("#results").value += num;
+}
+  
+function clearResults() {
+    document.querySelector("#results").value = "";
+}
+  
+function addition() {
+    document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+    document.querySelector("#results").value += "-";
+}
+
+function multiplication() {
+    document.querySelector("#results").value += "*";
+}
+
+function division() {
+    document.querySelector("#results").value += "/";
+}
+
+function changeSigns() {
+    var val = document.querySelector("#results").value;
+    if (val.charAt(0) === '-'){
+        document.querySelector("#results").value = val.slice(1);
+    }
+    else{
+        document.querySelector("#results").value = '-' + document.querySelector("#results").value;
+    }
+}
+
+function equals() {
+    document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+  
+function deleteLast() {
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+}
